91fb9ab2e77c388b8b7f5f068d07ddcdb974ae9d
Mark Schubert <mark@oifvc.com>
2026-04-15 11:36:18 +1000

    Fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-const http = require('http'); 
-const fs = require('fs'); 
-const path = require('path'); 
-const PORT = process.env.PORT || 3000; 
-http.createServer((req, res) => { 
-  res.writeHead(200, {'Content-Type': 'text/html'}); 
-  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res); 
+}).listen(PORT,function(){ console.log('ok'); }); ) 
